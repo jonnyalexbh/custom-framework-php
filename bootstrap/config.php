930fdb2f9ffe2545fd65d8d1c854075ac76b1ece
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-11-16 23:32:32 -0500

    inject doctrine into the HomeController controller builder

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -7,9 +7,10 @@ return [
     'config.database' => function () {
         return parse_ini_file(base_path('app/Config/database.ini'));
     },
-    HomeController::class => function () {
-        return new HomeController;
-    },
+    HomeController::class => \DI\create()->constructor(\DI\get(Doctrine::class)),
+    // HomeController::class => function (\Psr\Container\ContainerInterface $container) {
+    //     return new HomeController($container->get(Doctrine::class));
+    // },
     Doctrine::class => function (\Psr\Container\ContainerInterface $container) {
       return new Doctrine($container);
     },
