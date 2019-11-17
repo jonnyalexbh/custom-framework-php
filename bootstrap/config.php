30e1b0723c33f2a69124a6dd6d7a205d674031e9
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-11-16 23:04:10 -0500

    database configuration file and container load

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -3,6 +3,9 @@
 use Application\Controllers\HomeController;
 
 return [
+    'config.database' => function () {
+        return parse_ini_file(base_path('app/Config/database.ini'));
+    },
     HomeController::class => function () {
         return new HomeController;
     },
