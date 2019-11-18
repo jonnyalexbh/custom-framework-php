fb82179626d9496e210b35c162e4151a5cb06322
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-11-17 21:52:35 -0500

    develop a provider to use templates with Twig

---
+++ app/Providers/View.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Application\Providers;
+
+use Application\Utils\TwigFunctions;
+
+class View
+{
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * View constructor.
+     */
+    public function __construct()
+    {
+        $loader = new \Twig_Loader_Filesystem(base_path('resources/views'));
+        $twig = new \Twig_Environment($loader);
+
+        $twigFunctions = new \Twig_SimpleFunction(\TwigFunctions::class, function ($method, $params = []) {
+            return TwigFunctions::$method($params);
+        });
+        
+        $twig->addFunction($twigFunctions);
+
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param string $view
+     * @param array $data
+     *
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function render(string $view, array $data = []): string
+    {
+        return $this->twig->render($view, $data);
+    }
+}
--- bootstrap/config.php
+++ bootstrap/config.php
@@ -8,10 +8,8 @@ return [
         return parse_ini_file(base_path('app/Config/database.ini'));
     },
     HomeController::class => \DI\create()->constructor(\DI\get(Doctrine::class)),
-    // HomeController::class => function (\Psr\Container\ContainerInterface $container) {
-    //     return new HomeController($container->get(Doctrine::class));
-    // },
     Doctrine::class => function (\Psr\Container\ContainerInterface $container) {
       return new Doctrine($container);
     },
+    View::class => \DI\create(View::class),
 ];
